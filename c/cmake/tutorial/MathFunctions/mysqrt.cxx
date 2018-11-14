b2f0ac474b29d1c1cfbba991a368ba42c8a56300
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2018-11-14 21:35:43 +0100

    Next step in the tutorial System Introspection

--- c/cmake/tutorial/MathFunctions/mysqrt.cxx
+++ c/cmake/tutorial/MathFunctions/mysqrt.cxx
@@ -1,6 +1,11 @@
 #include "MathFunctions.h"
 
-double mysqrt(double inputValue)
+double mysqrt(double x)
 {
-  return sqrt(inputValue);
+// if we have both log and exp then use them
+#if defined (HAVE_LOG) && defined (HAVE_EXP)
+  return exp(log(x)*0.5);
+#else // otherwise use an iterative approach
+  return sqrt(x);
+#endif
 }
