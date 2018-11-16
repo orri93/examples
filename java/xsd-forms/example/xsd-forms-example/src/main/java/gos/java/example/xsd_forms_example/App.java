8db77da4ff3f1966092d8ce68abf07f5acc60936
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.com>
2018-11-16 09:45:06 +0100

    Adding more examples

---
+++ java/xsd-forms/example/xsd-forms-example/src/main/java/gos/java/example/xsd_forms_example/App.java
@@ -0,0 +1,13 @@
+package gos.java.example.xsd_forms_example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
