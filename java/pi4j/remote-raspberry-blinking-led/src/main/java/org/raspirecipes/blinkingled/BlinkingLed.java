272e3083d8ce874f91ffc3af2c5dc6eb70aba4be
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2018-12-10 09:33:23 +0100

    Update some examples

--- java/pi4j/remote-raspberry-blinking-led/src/main/java/org/raspirecipes/blinkingled/BlinkingLed.java
+++ java/pi4j/remote-raspberry-blinking-led/src/main/java/org/raspirecipes/blinkingled/BlinkingLed.java
@@ -5,11 +5,12 @@ import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.RaspiPin;
 
-public class BliningLed {
+public class BlinkingLed {
   public static void main( String[] args ) throws InterruptedException
   {
       GpioController gpio = GpioFactory.getInstance();
-      GpioPinDigitalOutput pin0 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00);
+      GpioPinDigitalOutput pin0 =
+      		gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05);
       boolean ledOn = true;
       for(int i = 0; i < 10; i++) {
            pin0.setState(ledOn);
