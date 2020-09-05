a54aef781ee4dcb77312962f144c7d3b7a19d095
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2020-09-05 23:39:41 +0200

    Add in-app navigation with routing to the Tour of Heroes Angular application see https://angular.io/tutorial/toh-pt5

--- web/angular-tour-of-heroes/src/app/hero.service.ts
+++ web/angular-tour-of-heroes/src/app/hero.service.ts
@@ -7,9 +7,7 @@ import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({providedIn: 'root'})
 
 export class HeroService {
 
@@ -19,5 +17,11 @@ export class HeroService {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 }
