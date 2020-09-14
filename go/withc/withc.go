80af6785f7c2fc52a609a8a38817803488259f59
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2020-09-14 07:47:34 +0200

    Update With C Go Experiment

--- go/withc/withc.go
+++ go/withc/withc.go
@@ -1,7 +1,7 @@
-package main
+package withc
 
 // #cgo CFLAGS: -I../../include
-// #cgo LDFLAGS: -L../../artifacts/RelWithDebInfo/lib -llibgosforgo
+// #cgo LDFLAGS: -L../../lib -llibgosforgo -Wl,-rpath=../../lib
 // #include <stdlib.h>
 // #include <gos/forgo.h>
 import "C"
