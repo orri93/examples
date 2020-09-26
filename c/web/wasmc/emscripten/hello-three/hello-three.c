30f24b0e53c63088a72f0717d0b4c629999aa194
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2020-09-26 19:16:51 +0200

    Update Web Assembly example with Compilation information

--- c/web/wasmc/emscripten/hello-three/hello-three.c
+++ c/web/wasmc/emscripten/hello-three/hello-three.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <emscripten/emscripten.h>
 
+/*
+ * 
+ * Calling a custom function defined in C
+ * 
+ * https://developer.mozilla.org/en-US/docs/WebAssembly/C_to_wasm
+ * 
+ * emcc -o hello-three.html hello-three.c -O3 -s WASM=1 --shell-file html_template/shell_minimal.html -s NO_EXIT_RUNTIME=1 -s "EXTRA_EXPORTED_RUNTIME_METHODS=['ccall']"
+ *
+ */
+
 int main(int argc, char** argv) {
   printf("Hello World\n");
 }
@@ -9,9 +19,9 @@ int main(int argc, char** argv) {
 extern "C" {
 #endif
 
-  void EMSCRIPTEN_KEEPALIVE myFunction(int argc, char** argv) {
-    printf("MyFunction Called\n");
-  }
+void EMSCRIPTEN_KEEPALIVE myFunction(int argc, char** argv) {
+  printf("MyFunction Called\n");
+}
 
 #ifdef __cplusplus
 }
