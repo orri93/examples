bc8624ad3733fe47475f074600a9c70b0502c9d0
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2020-10-26 14:50:46 +0100

    Adding SciChart, Go regex and an Angular

---
+++ go/regex/regex.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+  "fmt"
+  "regexp"
+)
+
+func main() {
+  // Compile the expression once, usually at init time.
+	// Use raw strings to avoid having to quote the backslashes.
+	// See https://golang.org/pkg/regexp/
+	var validID = regexp.MustCompile(`^[a-z]+\[[0-9]+\]$`)
+
+	fmt.Println(validID.MatchString("adam[23]"))
+	fmt.Println(validID.MatchString("eve[7]"))
+	fmt.Println(validID.MatchString("Job[48]"))
+	fmt.Println(validID.MatchString("snakey"))
+}
