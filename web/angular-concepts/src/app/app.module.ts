86bba048b8bb9441adbf00d14b87576067f441ef
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2021-01-30 12:20:44 +0100

    Adding a component to the Angular concept example

--- web/angular-concepts/src/app/app.module.ts
+++ web/angular-concepts/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
@@ -13,10 +14,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 
+import { HighchartsChartComponent } from 'highcharts-angular';
+
 import { HpComponent } from './hp/hp.component';
 import { P1Component } from './p1/p1.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { P2Component } from './p2/p2.component';
+import { C1Component } from './c1/c1.component';
 
 export function initializeApp(appConfiguration: AppConfiguration): any {
   return () => appConfiguration.load();
@@ -27,7 +30,8 @@ export function initializeApp(appConfiguration: AppConfiguration): any {
     AppComponent,
     HpComponent,
     P1Component,
-    P2Component
+    P2Component,
+    C1Component
   ],
   imports: [
     BrowserModule,
@@ -36,6 +40,7 @@ export function initializeApp(appConfiguration: AppConfiguration): any {
     HttpClientModule,
     HttpClientXsrfModule,
     BrowserAnimationsModule,
+    HighchartsChartComponent,
     MatSliderModule,
     MatButtonModule,
     MatInputModule,
---
+++ web/angular-concepts/src/app/c1/c1.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
+
+@Component({
+  selector: 'app-c1',
+  templateUrl: './c1.component.html',
+  styleUrls: ['./c1.component.css']
+})
+export class C1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
