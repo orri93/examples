2b9c4c97710a2a0e71ce21f08da2d1b029db60dc
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2021-02-03 08:44:56 +0100

    Adding more examples to the Angular Concept Example Project

--- web/angular-concepts/src/app/m0/m0.module.ts
+++ web/angular-concepts/src/app/m0/m0.module.ts
@@ -9,13 +9,15 @@ import { M0c1Component } from './m0c1/m0c1.component';
 import { M0c2Component } from './m0c2/m0c2.component';
 import { M0c0Component } from './m0c0/m0c0.component';
 import { M0c3Component } from './m0c3/m0c3.component';
+import { M0c4Component } from './m0c4/m0c4.component';
 
 @NgModule({
   declarations: [
     M0c0Component,
     M0c1Component,
     M0c2Component,
-    M0c3Component
+    M0c3Component,
+    M0c4Component
   ],
   imports: [
     CommonModule,
@@ -27,7 +29,8 @@ import { M0c3Component } from './m0c3/m0c3.component';
     M0c0Component,
     M0c1Component,
     M0c2Component,
-    M0c3Component
+    M0c3Component,
+    M0c4Component
   ],
 })
 export class M0Module { }
---
+++ web/angular-concepts/src/app/m0/m0c4/m0c4.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-m0c4',
+  templateUrl: './m0c4.component.html',
+  styleUrls: ['./m0c4.component.css']
+})
+export class M0c4Component implements OnInit {
+  componentName = 'Module no. 0 - Component no. 4';
+
+  first = 'First';
+  second = 'Second';
+  third = 'Third';
+  fourth = 'Forth';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
