7193daf82236912d7dd92206219c984275dc44da
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2021-07-23 15:17:17 +0200

    Updating the Java OPC UA Example

--- java/opcua/simple-client-read/src/main/java/gos/example/opcua/SimpleClientRead.java
+++ java/opcua/simple-client-read/src/main/java/gos/example/opcua/SimpleClientRead.java
@@ -1,7 +1,13 @@
 package gos.example.opcua;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class SimpleClientRead {
+  private final Logger logger = LoggerFactory.getLogger(getClass());
+
   public static void main(String[] args) {
+    logger.info("StartTime={}", value.getValue().getValue());
     System.out.println("Hello, World!");
   }
 }
