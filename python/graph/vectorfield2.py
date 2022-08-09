0ba583d32adb2f1d7e7834db9bf87d56f66bd335
Geirmundur Orri Sigurdsson <orri93@gmail.com>
2022-08-09 10:16:37 +0200

    Fix code format

--- python/graph/vectorfield2.py
+++ python/graph/vectorfield2.py
@@ -11,19 +11,19 @@ import matplotlib.pyplot as plt
 
 # Meshgrid
 x, y = np.meshgrid(np.linspace(-5, 5, 10), np.linspace(-5, 5, 10))
-  
+
 # Directional vectors
 u = -y/np.sqrt(x**2 + y**2)
 v = x/(x**2 + y**2)
-  
+
 # Plotting Vector Field with QUIVER
 plt.quiver(x, y, u, v, color='g')
 plt.title('Vector Field')
-  
+
 # Setting x, y boundary limits
 plt.xlim(-7, 7)
 plt.ylim(-7, 7)
-  
+
 # Show plot with grid
 plt.grid()
 plt.show()
