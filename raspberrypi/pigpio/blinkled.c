e822759eec0e2f2844828cf7387b7e699c297ae5
Geirmundur Orri Sigurdsson <orri93@gmail.com>
2024-04-27 14:37:10 -0500

    Improve the Pi GPIO example

--- raspberrypi/pigpio/blinkled.c
+++ raspberrypi/pigpio/blinkled.c
@@ -2,7 +2,7 @@
  * Raspberry Pi LED Blinking using C PiGPIO library
  *
  * Compile:
- *   gcc -Wall -pthread -o blinkled blinkled.c -lpigpio
+ *   gcc -Wall -pthread -o blinkled blinkled.c -lpigpio -lm
  *
  * Usage:
  *   ./blinkled
@@ -11,12 +11,30 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <math.h>
+
 #include <pigpio.h>
 
 #define LED_PIN 26
 
+volatile sig_atomic_t quit;
+
+void sighandler(int s) {
+  quit = 1;
+}
+
 int main(int argc, char *argv[]) {
-  int r, i;
+  int r, i, n;
+  double x, y;
+
+  quit = 0;
+
+  signal(SIGINT, sighandler);
+  signal(SIGTERM, sighandler);
+#ifdef SIGBREAK
+  signal(SIGBREAK, sighandler);
+#endif
 
   r = gpioInitialise();
   if (r < 0) {
@@ -26,14 +44,34 @@ int main(int argc, char *argv[]) {
 
   gpioSetMode(LED_PIN, PI_OUTPUT);
 
-  for (i = 0; i < 10; i++) {
-    printf("Number: %d\n", i);
+  for (i = 0; i < 4; i++) {
+    /* printf("Number: %d\n", i); */
     gpioWrite(LED_PIN, 1);
     gpioDelay(300000);
     gpioWrite(LED_PIN, 0);
     gpioDelay(300000);
+
   }
 
+  for (i = 0; i < 255; i++) {
+    gpioPWM(LED_PIN, i);
+    gpioDelay(10000);
+  }
+
+  for (x = 0.0; x < 628.0; x += 0.1) {
+    y = sin(x);
+    n = (int)((1.0 + y) * 127);
+    /* printf("Number for PWD: %d\ni", n); */
+    gpioPWM(LED_PIN, n);
+    gpioDelay(10000);
+    if (quit) {
+      break;
+    }
+  }
+
+  gpioPWM(LED_PIN, 0);
+
+  printf("Terminating the GPIO\n");
   gpioTerminate();
 
   return EXIT_SUCCESS;
